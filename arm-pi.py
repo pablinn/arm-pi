95c0703b6b3382488a034a61c7117d6b33dc268e
pablinn <penalve.sergio@yahoo.com.ar>
2016-08-27 21:35:56 -0300

    arm-pi para geany

--- arm-pi.py
+++ arm-pi.py
@@ -9,37 +9,44 @@ arch=sys.argv[1]+'/'+sys.argv[2]
 cc_as="arm-linux-gnueabihf-as"
 cc_gcc="arm-linux-gnueabihf-gcc"
 
+try:
+	if (sys.argv[3]=='s'):
+		#compila un archivo assembler arm a un obj-elf-arm
+		proc = Popen([cc_as ,'-mthumb', '-o', arch+'.o' , arch + '.s'] , stdin = PIPE , stdout = PIPE)
+		proc.stdout.readline()
+		proc.stdout.flush()
 
-proc = Popen([cc_as ,'-mthumb', '-o', arch+'.o' , arch + '.s'] , stdin = PIPE , stdout = PIPE)
-proc.stdout.readline()
-proc.stdout.flush()
+		#linkea el obj-elf-arm a un bin-elf-arm
+		proc1=Popen([cc_gcc,'-o',arch,arch+'.o'], stdin = PIPE, stdout = PIPE)
+		proc1.stdout.readline()
+		proc1.stdout.flush()
+		print "Compilado para ARM cross-compiler -->"+cc_gcc+"\n"
+        	print "Fuente"+arch+".s"
+
+	if (sys.argv[3]=='c'):
+		#linkea el obj-elf-arm a un bin-elf-arm
+        	proc1=Popen([cc_gcc,'-o',arch,arch+'.c'], stdin = PIPE, stdout = PIPE)
+       		proc1.stdout.readline()
+        	proc1.stdout.flush()
+		print "Compilado para ARM cross-compiler -->"+cc_gcc+"\n"
+       		print "Fuente -->"+arch+".c"
 
 
-proc1=Popen([cc_gcc,'-o',arch,arch+'.o'], stdin = PIPE, stdout = PIPE)
-proc1.stdout.readline()
-proc1.stdout.flush()
+	if (sys.argv[4]=='rpi'):
+		#envia el archivo bin-elf-arm a la rpi por shell remoto SCP
+		host='pi@192.168.43.222'
+		destino=':/home/pi/pru-arm'
+		proc3 = Popen(['scp', sys.argv[2] ,  host +  destino] , stdout = PIPE, stdin = PIPE)
+		proc3.stdout.readline()
+		proc3.stdout.flush()
+		print "Archivo enviado -->"+host+destino
 
+except IndexError as e:
+	print e
+
+"""no funcion
 proc4=Popen(['rm','-vf',sys.argv[1]+'/*.o'])
-#proc4.stdout.readline()
-#proc4.stdout.flush()
-
-
-host='pi@192.168.43.222'
-destino=':/home/pi/pru-arm'
-
-print 'scp', sys.argv[2] ,  host +  destino
-print 'rm','-vf',sys.argv[1]+'/*.o'
-
-proc3 = Popen(['scp', sys.argv[2] ,  host +  destino] , stdout = PIPE, stdin = PIPE)
-proc3.stdout.readline()
-proc3.stdout.flush()
-
-
-
-""" arm-linux-gnueabihf-as -mthumb -o %e.o %f
-arm-linux-gnueabihf-gcc -o %e %e.o 
-arm-linux-gnueabihf-gcc -o %e %e.c 
-rm -vf *.o
-envia a la pi
-entrara y lo ejecutara
+proc4.stdout.readline()
+proc4.stdout.flush()
 """
+
---
+++ prueba2.s
@@ -0,0 +1,9 @@
+.global main
+
+main:
+     mov r1,#5
+     mov r2,#7
+     add r0,r1,r2
+     bx lr
+     
+     
